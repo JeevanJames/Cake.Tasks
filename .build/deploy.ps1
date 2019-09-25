cc0d17baa6ac1a06488f293c069f4f02f12d2c45
Jeevan James <jeevanjj@live.com>
2019-09-25 20:19:03 +0530

    Update deployment ci script

--- .build/deploy.ps1
+++ .build/deploy.ps1
@@ -6,3 +6,9 @@ dotnet nuget push ./src/Cake.Tasks.Core/bin/Release/Cake.Tasks.Core.$env:APPVEYO
 
 dotnet pack ./src/Cake.Tasks.DotNetCore/Cake.Tasks.DotNetCore.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
 dotnet nuget push ./src/Cake.Tasks.DotNetCore/bin/Release/Cake.Tasks.DotNetCore.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
+
+dotnet pack ./src/Cake.Tasks.Octopus/Cake.Tasks.Octopus.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
+dotnet nuget push ./src/Cake.Tasks.Octopus/bin/Release/Cake.Tasks.Octopus.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
+
+dotnet pack ./src/Cake.Tasks.Sonar/Cake.Tasks.Sonar.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
+dotnet nuget push ./src/Cake.Tasks.Sonar/bin/Release/Cake.Tasks.Sonar.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
