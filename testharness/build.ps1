f7b7312c5c50bb615f498f51c7666eeff1c73587
Jeevan James <jeevanjj@live.com>
2019-09-28 14:08:51 +0530

    Update build.ps1

--- testharness/build.ps1
+++ testharness/build.ps1
@@ -252,6 +252,6 @@ $cakeArguments += $ScriptArgs
 
 # Start Cake
 Write-Host "Running build script..."
-Invoke-Expression "& $CAKE_EXE_INVOCATION $Script --bootstrap"
+# Invoke-Expression "& $CAKE_EXE_INVOCATION $Script --bootstrap"
 Invoke-Expression "& $CAKE_EXE_INVOCATION $($cakeArguments -join " ")"
 exit $LASTEXITCODE
