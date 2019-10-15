0f5c5a7c0476c80127fc857ba2748cb71729fd74
Jeevan James <jeevanjj@live.com>
2019-10-16 04:30:06 +0530

    Fix test harness init script

--- testharness/init.ps1
+++ testharness/init.ps1
@@ -7,5 +7,5 @@ Copy-Item -Path ..\src\Cake.Tasks.Module\bin\Debug\netstandard2.0\*.dll -Destina
 
 Copy-Item -Path ..\src\Cake.Tasks.Core\bin\Debug\netstandard2.0\Cake.Tasks.Core.dll -Destination .\tools\
 Copy-Item -Path ..\plugin\Cake.Tasks.GitVersion\bin\Debug\netstandard2.0\Cake.Tasks.GitVersion.dll -Destination .\tools\
-Copy-Item -Path ..\src\Cake.Tasks.DotNetCore\bin\Debug\netstandard2.0\Cake.Tasks.DotNetCore.dll -Destination .\tools\
-Copy-Item -Path ..\src\Cake.Tasks.Local\bin\Debug\netstandard2.0\Cake.Tasks.Local.dll -Destination .\tools\
+Copy-Item -Path ..\plugin\Cake.Tasks.DotNetCore\bin\Debug\netstandard2.0\Cake.Tasks.DotNetCore.dll -Destination .\tools\
+Copy-Item -Path ..\plugin\Cake.Tasks.Local\bin\Debug\netstandard2.0\Cake.Tasks.Local.dll -Destination .\tools\
