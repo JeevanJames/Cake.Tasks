1eb02192fd33f817c563f188dbbc638a1cd93949
Jeevan James <jeevanjj@live.com>
2019-10-16 04:26:36 +0530

    Update test harness init script

--- testharness/init.ps1
+++ testharness/init.ps1
@@ -9,6 +9,3 @@ Copy-Item -Path ..\src\Cake.Tasks.Core\bin\Debug\netstandard2.0\Cake.Tasks.Core.
 Copy-Item -Path ..\plugin\Cake.Tasks.GitVersion\bin\Debug\netstandard2.0\Cake.Tasks.GitVersion.dll -Destination .\tools\
 Copy-Item -Path ..\src\Cake.Tasks.DotNetCore\bin\Debug\netstandard2.0\Cake.Tasks.DotNetCore.dll -Destination .\tools\
 Copy-Item -Path ..\src\Cake.Tasks.Local\bin\Debug\netstandard2.0\Cake.Tasks.Local.dll -Destination .\tools\
-Copy-Item -Path ..\src\Cake.Tasks.Octopus\bin\Debug\netstandard2.0\Cake.Tasks.Octopus.dll -Destination .\tools\
-Copy-Item -Path ..\src\Cake.Tasks.Env.Tfs\bin\Debug\netstandard2.0\Cake.Tasks.Env.Tfs.dll -Destination .\tools\
-# Copy-Item -Path ..\src\Cake.Tasks.Sonar\bin\Debug\netstandard2.0\Cake.Tasks.Sonar.dll -Destination .\tools\
