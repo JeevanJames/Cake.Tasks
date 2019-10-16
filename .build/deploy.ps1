c5738fa10d8d952c93e16a2a57672977c6de56af
Jeevan James <jeevanjj@live.com>
2019-10-16 11:19:11 +0530

    Improve output of deploy script

--- .build/deploy.ps1
+++ .build/deploy.ps1
@@ -9,22 +9,30 @@ dotnet pack ./src/Cake.Tasks.Core/Cake.Tasks.Core.csproj --include-symbols --inc
 dotnet nuget push ./src/Cake.Tasks.Core/bin/Release/Cake.Tasks.Core.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
 
 # Cake Tasks GitVersion plugin
+Write-Host "======================================"
 Write-Host "Publishing Cake Tasks GitVersion addin" -ForegroundColor Magenta
+Write-Host "======================================"
 dotnet pack ./plugin/Cake.Tasks.GitVersion/Cake.Tasks.GitVersion.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
 dotnet nuget push ./plugin/Cake.Tasks.GitVersion/bin/Release/Cake.Tasks.GitVersion.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
 
 # Cake Tasks .NET Core plugin
+Write-Host "====================================="
 Write-Host "Publishing Cake Tasks .NET Core addin" -ForegroundColor Magenta
+Write-Host "====================================="
 dotnet pack ./plugin/Cake.Tasks.DotNetCore/Cake.Tasks.DotNetCore.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
 dotnet nuget push ./plugin/Cake.Tasks.DotNetCore/bin/Release/Cake.Tasks.DotNetCore.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
 
 # Cake Tasks Local plugin
+Write-Host "================================="
 Write-Host "Publishing Cake Tasks local addin" -ForegroundColor Magenta
+Write-Host "================================="
 dotnet pack ./plugin/Cake.Tasks.Local/Cake.Tasks.Local.csproj --include-symbols --include-source -c Release /p:Version=$env:APPVEYOR_BUILD_VERSION
 dotnet nuget push ./plugin/Cake.Tasks.Local/bin/Release/Cake.Tasks.Local.$env:APPVEYOR_BUILD_VERSION.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY -ss $env:MYGET_SYMBOLS_FEED -sk $env:MYGET_SYMBOLS_APIKEY
 
 # Meta package
+Write-Host "========================================"
 Write-Host "Publishing Cake Tasks addins metapackage" -ForegroundColor Magenta
+Write-Host "========================================"
 New-Item -ItemType Directory -Path ./metapackage/ref/netstandard2.0
 New-Item -ItemType Directory -Path ./metapackage/lib/netstandard2.0
 
