cb7d61901ee467d70b98c2a71b1612be27c4d614
Jeevan James <jeevanjj@live.com>
2019-10-20 23:46:32 +0530

    Update deploy script

--- .build/deploy.ps1
+++ .build/deploy.ps1
@@ -21,17 +21,6 @@ PublishPackage "plugin" "Cake.Tasks.DotNetCore"
 
 # Meta package
 WriteBanner "Publishing Cake Tasks addins metapackage"
-New-Item -ItemType Directory -Path ./metapackage/ref/netstandard2.0
-New-Item -ItemType Directory -Path ./metapackage/lib/netstandard2.0
-
-Copy-Item -Path ./src/Cake.Tasks.Core/bin/Release/netstandard2.0/Cake.Tasks.Core.dll -Destination .\metapackage\ref\netstandard2.0
-Copy-Item -Path ./plugin/Cake.Tasks.DotNetCore/bin/Release/netstandard2.0/Cake.Tasks.DotNetCore.dll -Destination .\metapackage\ref\netstandard2.0
-Copy-Item -Path ./plugin/Cake.Tasks.GitVersion/bin/Release/netstandard2.0/Cake.Tasks.GitVersion.dll -Destination .\metapackage\ref\netstandard2.0
-
-Copy-Item -Path ./src/Cake.Tasks.Core/bin/Release/netstandard2.0/Cake.Tasks.Core.dll -Destination .\metapackage\lib\netstandard2.0
-Copy-Item -Path ./plugin/Cake.Tasks.DotNetCore/bin/Release/netstandard2.0/Cake.Tasks.DotNetCore.dll -Destination .\metapackage\lib\netstandard2.0
-Copy-Item -Path ./plugin/Cake.Tasks.GitVersion/bin/Release/netstandard2.0/Cake.Tasks.GitVersion.dll -Destination .\metapackage\lib\netstandard2.0
-
 ((Get-Content -path ./metapackage/CakeTasks.nuspec -Raw) -replace '0.1.0',$env:APPVEYOR_BUILD_VERSION) | Set-Content -Path ./metapackage/CakeTasks.nuspec
 nuget pack ./metapackage/CakeTasks.nuspec -OutputDirectory ./metapackage -OutputFileNamesWithoutVersion
 dotnet nuget push ./metapackage/Cake.Tasks.JeevanJames.nupkg -s $env:MYGET_FEED -k $env:MYGET_APIKEY
