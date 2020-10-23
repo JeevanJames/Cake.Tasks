c46299d3cfad959c7482bbbcf6887171a66fcddc
Jeevan James <jeevanjj@live.com>
2020-10-23 13:32:16 +0530

    Log extension method

--- src/Cake.Tasks.Core/LogExtensions.cs
+++ src/Cake.Tasks.Core/LogExtensions.cs
@@ -28,14 +28,39 @@ namespace Cake.Tasks.Core
     {
         public static void LogInfo(this ICakeContext ctx, string message, [CallerMemberName] string caller = "")
         {
-            string fullMessage = $"\u001b[37;1m\u001b[44;1m{caller}\u001b[0m {message}";
-            ctx.Log.Information(fullMessage);
+            ctx.Log.Information($"{InfoPrefix}{caller}{Reset} {Info}{message}{Reset}");
         }
 
         public static void LogWarning(this ICakeContext ctx, string message, [CallerMemberName] string caller = "")
         {
-            string fullMessage = $"\u001b[30;1m\u001b[43;1m{caller}\u001b[0m {message}";
-            ctx.Log.Information(fullMessage);
+            ctx.Log.Warning($"{WarningPrefix}{caller}{Reset} {Warning}{message}{Reset}");
         }
+
+        public static void LogVerbose(this ICakeContext ctx, string message, [CallerMemberName] string caller = "")
+        {
+            ctx.Log.Verbose($"{VerbosePrefix}{caller}{Reset} {Verbose}{message}{Reset}");
+        }
+
+        private const string Escape = "\u001b[";
+
+        private const string Reset = Escape + "0m";
+
+        private const string InfoPrefixBg = Escape + "44;1m"; // Bright blue bg
+        private const string InfoPrefixFg = Escape + "37;1m"; // Bright white fg
+        private const string InfoPrefix = InfoPrefixFg + InfoPrefixBg;
+        private const string InfoFg = Escape + "34;1m"; // Bright blue fg
+        private const string Info = InfoFg;
+
+        private const string WarningPrefixBg = Escape + "43;1m"; // Bright yellow bg
+        private const string WarningPrefixFg = Escape + "30;1m"; // Bright black fg
+        private const string WarningPrefix = WarningPrefixFg + WarningPrefixBg;
+        private const string WarningFg = Escape + "33;1m"; // Bright yellow fg
+        private const string Warning = WarningFg;
+
+        private const string VerbosePrefixBg = Escape + "46;1m"; // Bright cyan bg
+        private const string VerbosePrefixFg = Escape + "37;1m"; // Bright white fg
+        private const string VerbosePrefix = VerbosePrefixFg + VerbosePrefixBg;
+        private const string VerboseFg = Escape + "36;1m"; // Bright cyan fg
+        private const string Verbose = VerboseFg;
     }
 }
