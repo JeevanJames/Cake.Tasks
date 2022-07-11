1101e08eef1ef6a588edbf201448454bc5e8237e
Jeevan James <jeevanjj@live.com>
2022-07-11 15:26:03 +0530

    Update file headers

--- src/Cake.Tasks.Core/Attributes/ConfigTaskOrder.cs
+++ src/Cake.Tasks.Core/Attributes/ConfigTaskOrder.cs
@@ -1,24 +1,28 @@
-﻿namespace Cake.Tasks.Core
+﻿// Cake Tasks framework for Cake Build
+// Copyright (c) 2019-2022 Jeevan James
+// This file is licensed to you under the Apache License, Version 2.0.
+// See the LICENSE file in the project root for more information.
+
+namespace Cake.Tasks.Core;
+
+public static class ConfigTaskOrder
 {
-    public static class ConfigTaskOrder
-    {
-        public const int Default = 0;
+    public const int Default = 0;
 
-        /// <summary>
-        ///     Priority config tasks should run after all the normal config tasks, but before any
-        ///     CI system tasks.
-        /// </summary>
-        public const int Priority = 100;
+    /// <summary>
+    ///     Priority config tasks should run after all the normal config tasks, but before any
+    ///     CI system tasks.
+    /// </summary>
+    public const int Priority = 100;
 
-        /// <summary>
-        ///     Config tasks for CI systems should run after all the normal and priority tasks, but
-        ///     before any recipe tasks.
-        /// </summary>
-        public const int CiSystem = 1000;
+    /// <summary>
+    ///     Config tasks for CI systems should run after all the normal and priority tasks, but
+    ///     before any recipe tasks.
+    /// </summary>
+    public const int CiSystem = 1000;
 
-        /// <summary>
-        ///     Config tasks for recipes should always run last, as they are application-specific.
-        /// </summary>
-        public const int Recipe = int.MaxValue;
-    }
+    /// <summary>
+    ///     Config tasks for recipes should always run last, as they are application-specific.
+    /// </summary>
+    public const int Recipe = int.MaxValue;
 }
--- src/Cake.Tasks.Core/DumpExtensions.cs
+++ src/Cake.Tasks.Core/DumpExtensions.cs
@@ -1,21 +1,7 @@
-﻿#region --- License & Copyright Notice ---
-/*
-Cake Tasks
-Copyright 2019 Jeevan James
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-#endregion
+﻿// Cake Tasks framework for Cake Build
+// Copyright (c) 2019-2022 Jeevan James
+// This file is licensed to you under the Apache License, Version 2.0.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Collections;
