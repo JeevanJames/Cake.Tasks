49daaf38a2c0cb2e58af07fa1b369f45ec11f3c3
Jeevan James <jeevanjj@live.com>
2022-08-07 02:19:23 +0530

    More file-scoped namespaces

--- src/Cake.Tasks.Module/PluginLoaders/PluginLoader.cs
+++ src/Cake.Tasks.Module/PluginLoaders/PluginLoader.cs
@@ -15,128 +15,127 @@ using Cake.Core.Diagnostics;
 using Cake.Tasks.Config;
 using Cake.Tasks.Core;
 
-namespace Cake.Tasks.Module.PluginLoaders
+namespace Cake.Tasks.Module.PluginLoaders;
+
+/// <summary>
+///     Encapsulates the logic to load Cake.Tasks plugins.
+/// </summary>
+public abstract class PluginLoader
 {
-    /// <summary>
-    ///     Encapsulates the logic to load Cake.Tasks plugins.
-    /// </summary>
-    public abstract class PluginLoader
+    protected PluginLoader(string pluginsDir, ICakeLog log)
     {
-        protected PluginLoader(string pluginsDir, ICakeLog log)
+        PluginsDir = pluginsDir;
+        Log = log;
+    }
+
+    internal abstract IEnumerable<RegisteredTask> LoadPlugins();
+
+    protected string PluginsDir { get; }
+
+    protected ICakeLog Log { get; }
+
+    internal IEnumerable<RegisteredTask> FindPlugins(string dllFile)
+    {
+        Assembly assembly = Assembly.LoadFile(dllFile);
+
+        IEnumerable<TaskPluginAttribute> taskPlugins = assembly.GetCustomAttributes<TaskPluginAttribute>();
+
+        foreach (TaskPluginAttribute taskPlugin in taskPlugins)
         {
-            PluginsDir = pluginsDir;
-            Log = log;
-        }
+            Type taskPluginType = taskPlugin.PluginType;
+            Log.Verbose($"[Plugin Class] {taskPluginType.FullName}");
 
-        internal abstract IEnumerable<RegisteredTask> LoadPlugins();
-
-        protected string PluginsDir { get; }
-
-        protected ICakeLog Log { get; }
-
-        internal IEnumerable<RegisteredTask> FindPlugins(string dllFile)
-        {
-            Assembly assembly = Assembly.LoadFile(dllFile);
-
-            IEnumerable<TaskPluginAttribute> taskPlugins = assembly.GetCustomAttributes<TaskPluginAttribute>();
-
-            foreach (TaskPluginAttribute taskPlugin in taskPlugins)
+            // Get all public static methods from the task plugin type.
+            MethodInfo[] methods = taskPluginType.GetMethods(BindingFlags.Static | BindingFlags.Public);
+            foreach (MethodInfo method in methods)
             {
-                Type taskPluginType = taskPlugin.PluginType;
-                Log.Verbose($"[Plugin Class] {taskPluginType.FullName}");
+                // Gets any task attributes on the method.
+                IEnumerable<BaseTaskAttribute> taskAttributes = method.GetCustomAttributes<BaseTaskAttribute>(
+                    inherit: true);
+                if (!taskAttributes.Any())
+                    continue;
 
-                // Get all public static methods from the task plugin type.
-                MethodInfo[] methods = taskPluginType.GetMethods(BindingFlags.Static | BindingFlags.Public);
-                foreach (MethodInfo method in methods)
+                // Method signature should match a valid task method.
+                if (!IsValidPluginMethod(method))
                 {
-                    // Gets any task attributes on the method.
-                    IEnumerable<BaseTaskAttribute> taskAttributes = method.GetCustomAttributes<BaseTaskAttribute>(
-                        inherit: true);
-                    if (!taskAttributes.Any())
-                        continue;
+                    Log.Warning($"Method {taskPluginType.FullName}.{method.Name} is decorated with one or more task attributes, but does not have the correct signature, so it will not be considered. A valid task method should be a static or instance method that returns void and accepts a first parameter of type {typeof(ICakeContext).FullName} and an optional second parameter of type {typeof(TaskConfig).FullName}.");
+                    continue;
+                }
 
-                    // Method signature should match a valid task method.
-                    if (!IsValidPluginMethod(method))
+                Log.Verbose($"[Plugin Method] {taskPluginType.FullName}.{method.Name}");
+
+                // Go through each task attribute and create a RegisteredTask object
+                foreach (BaseTaskAttribute taskAttribute in taskAttributes)
+                {
+                    RegisteredTask registeredTask = new()
                     {
-                        Log.Warning($"Method {taskPluginType.FullName}.{method.Name} is decorated with one or more task attributes, but does not have the correct signature, so it will not be considered. A valid task method should be a static or instance method that returns void and accepts a first parameter of type {typeof(ICakeContext).FullName} and an optional second parameter of type {typeof(TaskConfig).FullName}.");
-                        continue;
+                        AttributeType = taskAttribute.GetType(),
+                        Method = method,
+                        CiSystem = taskAttribute.CiSystem,
+                        ContinueOnError = taskAttribute.ContinueOnError,
+                    };
+
+                    string envSuffix = taskAttribute.CiSystem is null ? string.Empty : $"-{taskAttribute.CiSystem}";
+                    string methodDetails = $"{registeredTask.Method.DeclaringType.FullName}.{registeredTask.Method.Name} ({registeredTask.Method.DeclaringType.Assembly.GetName().Name})";
+
+                    switch (taskAttribute)
+                    {
+                        case PipelineTaskAttribute attr:
+                            registeredTask.CoreTask = attr.PipelineTask;
+                            registeredTask.Name = $"_{attr.PipelineTask}-{method.Name}{envSuffix}";
+                            registeredTask.Description = $"{attr.PipelineTask} task - {methodDetails}";
+                            break;
+                        case BeforePipelineTaskAttribute attr:
+                            registeredTask.CoreTask = attr.PipelineTask;
+                            registeredTask.EventType = TaskEventType.BeforeTask;
+                            registeredTask.Name = $"_Before{attr.PipelineTask}-{method.Name}{envSuffix}";
+                            registeredTask.Description = $"Before {attr.PipelineTask} task - {methodDetails}";
+                            break;
+                        case AfterPipelineTaskAttribute attr:
+                            registeredTask.CoreTask = attr.PipelineTask;
+                            registeredTask.EventType = TaskEventType.AfterTask;
+                            registeredTask.Name = $"_After{attr.PipelineTask}-{method.Name}{envSuffix}";
+                            registeredTask.Description = $"After {attr.PipelineTask} task - {methodDetails}";
+                            break;
+                        case ConfigAttribute attr:
+                            registeredTask.Name = $"_Config-{method.Name}{envSuffix}";
+                            registeredTask.Description = $"Config task - {methodDetails}";
+                            registeredTask.Order = attr.Order;
+                            break;
+                        case TaskAttribute attr:
+                            registeredTask.Name = method.Name;
+                            registeredTask.Description = $"{attr.Description}{envSuffix} - {methodDetails}";
+                            registeredTask.RequiresConfig = attr.RequiresConfig;
+                            break;
                     }
 
-                    Log.Verbose($"[Plugin Method] {taskPluginType.FullName}.{method.Name}");
-
-                    // Go through each task attribute and create a RegisteredTask object
-                    foreach (BaseTaskAttribute taskAttribute in taskAttributes)
-                    {
-                        RegisteredTask registeredTask = new()
-                        {
-                            AttributeType = taskAttribute.GetType(),
-                            Method = method,
-                            CiSystem = taskAttribute.CiSystem,
-                            ContinueOnError = taskAttribute.ContinueOnError,
-                        };
-
-                        string envSuffix = taskAttribute.CiSystem is null ? string.Empty : $"-{taskAttribute.CiSystem}";
-                        string methodDetails = $"{registeredTask.Method.DeclaringType.FullName}.{registeredTask.Method.Name} ({registeredTask.Method.DeclaringType.Assembly.GetName().Name})";
-
-                        switch (taskAttribute)
-                        {
-                            case PipelineTaskAttribute attr:
-                                registeredTask.CoreTask = attr.PipelineTask;
-                                registeredTask.Name = $"_{attr.PipelineTask}-{method.Name}{envSuffix}";
-                                registeredTask.Description = $"{attr.PipelineTask} task - {methodDetails}";
-                                break;
-                            case BeforePipelineTaskAttribute attr:
-                                registeredTask.CoreTask = attr.PipelineTask;
-                                registeredTask.EventType = TaskEventType.BeforeTask;
-                                registeredTask.Name = $"_Before{attr.PipelineTask}-{method.Name}{envSuffix}";
-                                registeredTask.Description = $"Before {attr.PipelineTask} task - {methodDetails}";
-                                break;
-                            case AfterPipelineTaskAttribute attr:
-                                registeredTask.CoreTask = attr.PipelineTask;
-                                registeredTask.EventType = TaskEventType.AfterTask;
-                                registeredTask.Name = $"_After{attr.PipelineTask}-{method.Name}{envSuffix}";
-                                registeredTask.Description = $"After {attr.PipelineTask} task - {methodDetails}";
-                                break;
-                            case ConfigAttribute attr:
-                                registeredTask.Name = $"_Config-{method.Name}{envSuffix}";
-                                registeredTask.Description = $"Config task - {methodDetails}";
-                                registeredTask.Order = attr.Order;
-                                break;
-                            case TaskAttribute attr:
-                                registeredTask.Name = method.Name;
-                                registeredTask.Description = $"{attr.Description}{envSuffix} - {methodDetails}";
-                                registeredTask.RequiresConfig = attr.RequiresConfig;
-                                break;
-                        }
-
-                        yield return registeredTask;
-                    }
+                    yield return registeredTask;
                 }
             }
         }
-
-        protected bool IsValidPluginMethod(MethodInfo method)
-        {
-            ParameterInfo[] parameters = method.GetParameters();
-
-            // There can be 0 to 2 parameters
-            if (parameters.Length > 2)
-                return false;
-
-            // The method should return void.
-            if (method.ReturnType != typeof(void))
-                return false;
-
-            return parameters.Length switch
-            {
-                2 => typeof(ICakeContext).IsAssignableFrom(parameters[0].ParameterType)
-                   && parameters[1].ParameterType == typeof(TaskConfig),
-                1 => typeof(ICakeContext).IsAssignableFrom(parameters[0].ParameterType)
-                    || parameters[0].ParameterType == typeof(TaskConfig),
-                _ => true,
-            };
-        }
-
-        protected abstract Assembly ResolveAssembly(object sender, ResolveEventArgs args);
     }
+
+    protected bool IsValidPluginMethod(MethodInfo method)
+    {
+        ParameterInfo[] parameters = method.GetParameters();
+
+        // There can be 0 to 2 parameters
+        if (parameters.Length > 2)
+            return false;
+
+        // The method should return void.
+        if (method.ReturnType != typeof(void))
+            return false;
+
+        return parameters.Length switch
+        {
+            2 => typeof(ICakeContext).IsAssignableFrom(parameters[0].ParameterType)
+                && parameters[1].ParameterType == typeof(TaskConfig),
+            1 => typeof(ICakeContext).IsAssignableFrom(parameters[0].ParameterType)
+                || parameters[0].ParameterType == typeof(TaskConfig),
+            _ => true,
+        };
+    }
+
+    protected abstract Assembly ResolveAssembly(object sender, ResolveEventArgs args);
 }
--- src/Cake.Tasks.Module/PluginLoaders/ProductionPluginLoader.cs
+++ src/Cake.Tasks.Module/PluginLoaders/ProductionPluginLoader.cs
@@ -14,44 +14,43 @@ using System.Reflection;
 using Cake.Core.Diagnostics;
 using Cake.Tasks.Core.Internal;
 
-namespace Cake.Tasks.Module.PluginLoaders
+namespace Cake.Tasks.Module.PluginLoaders;
+
+public sealed class ProductionPluginLoader : PluginLoader
 {
-    public sealed class ProductionPluginLoader : PluginLoader
+    public ProductionPluginLoader(string pluginsDir, ICakeLog log)
+        : base(pluginsDir, log)
     {
-        public ProductionPluginLoader(string pluginsDir, ICakeLog log)
-            : base(pluginsDir, log)
-        {
-        }
+    }
 
-        internal override IEnumerable<RegisteredTask> LoadPlugins()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+    internal override IEnumerable<RegisteredTask> LoadPlugins()
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
 
-            foreach (string cakeTasksDir in AddinFinder.Find(PluginsDir))
+        foreach (string cakeTasksDir in AddinFinder.Find(PluginsDir))
+        {
+            //TODO: Need to support for framework targets
+            string dllDir = Path.Combine(cakeTasksDir, "lib", "netstandard2.0");
+            if (!Directory.Exists(dllDir))
+                continue;
+
+            string[] dllFiles = Directory.GetFiles(dllDir, "*.dll", SearchOption.TopDirectoryOnly);
+
+            foreach (string dllFile in dllFiles)
             {
-                //TODO: Need to support for framework targets
-                string dllDir = Path.Combine(cakeTasksDir, "lib", "netstandard2.0");
-                if (!Directory.Exists(dllDir))
-                    continue;
-
-                string[] dllFiles = Directory.GetFiles(dllDir, "*.dll", SearchOption.TopDirectoryOnly);
-
-                foreach (string dllFile in dllFiles)
-                {
-                    foreach (RegisteredTask dllPlugin in FindPlugins(dllFile))
-                        yield return dllPlugin;
-                }
+                foreach (RegisteredTask dllPlugin in FindPlugins(dllFile))
+                    yield return dllPlugin;
             }
         }
+    }
 
-        protected override Assembly ResolveAssembly(object sender, ResolveEventArgs args)
-        {
-            AssemblyName assemblyName = new(args.Name);
-            string assemblyPath = Directory
-                .GetFiles(PluginsDir, $"{assemblyName.Name}.dll", SearchOption.AllDirectories)
-                .FirstOrDefault();
-            Log.Verbose($"[Assembly Lookup] {assemblyName.Name}.dll");
-            return Assembly.LoadFile(assemblyPath);
-        }
+    protected override Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+    {
+        AssemblyName assemblyName = new(args.Name);
+        string assemblyPath = Directory
+            .GetFiles(PluginsDir, $"{assemblyName.Name}.dll", SearchOption.AllDirectories)
+            .FirstOrDefault();
+        Log.Verbose($"[Assembly Lookup] {assemblyName.Name}.dll");
+        return Assembly.LoadFile(assemblyPath);
     }
 }
